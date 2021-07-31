b879c3dd2b37d93966eee7e38e5530898390fde1
fachu000 <daniel.romero@uia.no>
2021-07-31 12:36:46 +0200

    added random number generator

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
+
 from experiment_set import AbstractExperimentSet
 from gfigure import GFigure
 from experiment_set import OUTPUT_DATA_FOLDER
 
 from utils import time_to_str, print_obj_attributes, instr
+
+from numpy.random import default_rng
+rng = default_rng()
