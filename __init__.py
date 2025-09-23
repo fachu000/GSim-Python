e667d58763eeaeb4f8d7cfd0529128acc61f1a91
Daniel Romero <fachu000@gmail.com>
2025-09-23 10:58:19 +0200

    GColorFormatter is public

--- __init__.py
+++ __init__.py
@@ -11,34 +11,35 @@ from numpy.random import default_rng
 rng = default_rng()
 
 
+class GColoredFormatter(logging.Formatter):
+    # Define color codes
+    COLORS = {
+        'DEBUG': '\033[94m',  # Blue
+        'INFO': '\033[92m',  # Green
+        'WARNING': '\033[93m',  # Yellow
+        'ERROR': '\033[91m',  # Red
+        'CRITICAL': '\033[95m',  # Magenta
+    }
+    RESET = '\033[0m'  # Reset color
+
+    def format(self, record):
+        # Add color to the log level name
+        levelname = record.levelname
+        if levelname in self.COLORS:
+            levelname_color = f"{self.COLORS[levelname]}{levelname}{self.RESET}"
+            record.levelname = levelname_color
+        return super().format(record)
+
+
 def init_gsim_logger():
-
-    class ColoredFormatter(logging.Formatter):
-        # Define color codes
-        COLORS = {
-            'DEBUG': '\033[94m',  # Blue
-            'INFO': '\033[92m',  # Green
-            'WARNING': '\033[93m',  # Yellow
-            'ERROR': '\033[91m',  # Red
-            'CRITICAL': '\033[95m',  # Magenta
-        }
-        RESET = '\033[0m'  # Reset color
-
-        def format(self, record):
-            # Add color to the log level name
-            levelname = record.levelname
-            if levelname in self.COLORS:
-                levelname_color = f"{self.COLORS[levelname]}{levelname}{self.RESET}"
-                record.levelname = levelname_color
-            return super().format(record)
 
     logger = logging.getLogger("gsim")
     logger.setLevel(logging.INFO)
 
     handler = logging.StreamHandler()
     handler.setLevel(logging.INFO)
-    formatter = ColoredFormatter('{levelname}:{module}:{lineno}: {message}',
-                                 style='{')
+    formatter = GColoredFormatter('{levelname}:{module}:{lineno}: {message}',
+                                  style='{')
     handler.setFormatter(formatter)
     if not logger.handlers:
         logger.addHandler(handler)
