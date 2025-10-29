4d60611029167f018ff9a2aabebdd605ba994731
Daniel Romero <fachu000@gmail.com>
2025-10-29 12:20:49 +0100

    Added imports in NeuralNet

--- include/neural_net/__init__.py
+++ include/neural_net/__init__.py
@@ -1,3 +1,4 @@
 from .defs import InputType, OutputType, TargetType, LossFunType
 from .neural_net import NeuralNet
-from .normalizers import Normalizer, DefaultNormalizer
+from .normalizers import FeatNormalizer, IdentityFeatNormalizer, \
+    StdFeatNormalizer, IntervalFeatNormalizer, MultiFeatNormalizer
